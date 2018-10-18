a4652adb6c22b002a445edf18d7594f5e090c32e
Tilo Werdin <tilo.werdin@gmail.com>
2018-10-18 19:58:58 +0200

    nice header and nav menu

---
+++ A1/js/navbar.js
@@ -0,0 +1,31 @@
+var showElements = false;
+
+function onResize() {
+	if (window.innerWidth > 922) {
+		var elems = document.getElementsByClassName("menu");
+		for (var elem in elems) {
+			elems[elem].style.display = 'table-cell';
+		}
+	}
+	if (window.innerWidth < 922) {
+		var elems = document.getElementsByClassName("menu");
+		for (var elem in elems) {
+			elems[elem].style.display = 'none';
+		}
+	}
+}
+
+function toggle_nav_elements() {
+	showElements = !showElements;
+	if (showElements) {
+		var elems = document.getElementsByClassName("menu");
+		for (var elem in elems) {
+			elems[elem].style.display = 'block';
+		}
+	} else {
+		var elems = document.getElementsByClassName("menu");
+		for (var elem in elems) {
+			elems[elem].style.display = 'none';
+		}
+	}
+}
