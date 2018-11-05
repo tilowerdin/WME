4e077cb1586e0d45108f3e5f9727a8e029b6e859
Maximilian Kindt <maximilian.kindt@mailbox.tu-dresden.de>
2018-11-05 10:23:23 +0100

    Dateien auf Loesung von A1 geupdatet

--- A2/js/navbar.js
+++ A2/js/navbar.js
@@ -1,31 +1,31 @@
 var showElements = false;
+var menuItems = 6;
 
+// since we are overriding the css display attributes in toggle_nav_elements, this is not possible doing in css
 function onResize() {
+	var elems = document.getElementsByClassName("menu-div");
 	if (window.innerWidth > 922) {
-		var elems = document.getElementsByClassName("menu");
-		// console.log(elems);
-		for (var elem in elems) {
+		for (var elem = 0; elem < menuItems; elem++) {
 			elems[elem].style.display = 'table-cell';
 		}
 	}
 	if (window.innerWidth < 922) {
-		var elems = document.getElementsByClassName("menu");
-		for (var elem in elems) {
+		for (var elem = 0; elem < menuItems; elem++) {
 			elems[elem].style.display = 'none';
 		}
 	}
 }
 
+// toggle the nav menu item list, usually when the screen width is small
 function toggle_nav_elements() {
 	showElements = !showElements;
+	var elems = document.getElementsByClassName("menu-div");
 	if (showElements) {
-		var elems = document.getElementsByClassName("menu");
-		for (var elem in elems) {
+		for (var elem = 0; elem < menuItems; elem++) {
 			elems[elem].style.display = 'block';
 		}
 	} else {
-		var elems = document.getElementsByClassName("menu");
-		for (var elem in elems) {
+		for (var elem = 0; elem < menuItems; elem++) {
 			elems[elem].style.display = 'none';
 		}
 	}
