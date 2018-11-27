609d58bf2551632a1fca5723d37a1e2532021ae3
Tilo Werdin <werdin@MacBook.fritz.box>
2018-11-27 20:28:01 +0100

    REST GET calls

--- A3/app.js
+++ A3/app.js
@@ -6,7 +6,6 @@ var sys = require('util');
 var path = require('path');
 var bodyParser = require('body-parser');
 var Converter = require("csvtojson").Converter;
-var csv = require("csvtojson");
 
 //register body-parser to handle json from res / req
 app.use( bodyParser.json() );
@@ -38,12 +37,87 @@ converter.then((obj) => {
 /**************************************************************************
 ********************** handle HTTP METHODS ***********************
 **************************************************************************/
-app.get("/item/:id",function (req,res) {
-    let id = req.params.id;
-    console.log(id);
+// GET call "/items"
+app.get("/items",function (req,res) {
     res.send(json);
-
 });
+
+// GET call "/items/id"
+app.get("/items/:id",function (req,res) {
+    let id = req.params.id;
+    var answer = "No such id " + id + " in database.";
+    for(var i = 0; i < json.length; i++) {
+        if(json[i]["id"] == id) {
+            answer = json[i];
+        }
+    }
+    
+    // sending message "ERROR" on failure
+    res.send(answer);
+});
+
+// GET call "/items/id1/id2"
+app.get("/items/:id1/:id2",function (req,res) {
+    let id1 = parseInt(req.params.id1);
+    let id2 = parseInt(req.params.id2);
+    var answer = "Range not possible.";
+    
+    if (id1 > 0 &&
+        id1 < id2
+    ) {
+        var i = 0;
+        for(; i < json.length; i++) {
+            if(parseInt(json[i]["id"]) == id1) {
+                answer = [];
+                break;
+            }
+        }
+        while (true) {
+            if (i >= json.length) {
+                answer = "Range not possible.";
+                break;
+            }
+            answer.push(json[i]);
+            if(parseInt(json[i]["id"]) == id2) {
+                break;
+            }
+            i++;
+        }
+    }
+    
+    res.send(answer);
+});
+
+// GET call "/properties"
+app.get("/properties",function (req,res) {
+    var answer = [];
+    
+    for(var key in json[0]) {
+        answer.push(key);
+    }
+
+    res.send(answer);
+});
+
+// GET call "/properties/num"
+app.get("/properties/:num",function (req,res) {
+    let num = parseInt(req.params.num);
+    var answer = "No such property available.";
+    
+    if(num >= 0) {
+        var i = 0;
+        for(var key in json[0]) {
+            if(i == num) {
+                answer = key;
+                break;
+            }
+            i++;
+        }
+    }
+
+    res.send(answer);
+});
+
 
 // DO NOT CHANGE!
 // bind server to port
